ec9c68e1fbabb1cdbe9470062ba6fbab56f0bd97
gogo <giorgio.crepaldi93@gmail.com>
2020-06-25 15:09:08 +0200

    mesh case set

--- cp-vton/train.py
+++ cp-vton/train.py
@@ -187,7 +187,7 @@ def main():
         train_gmm(opt, train_loader, model, board)
         save_checkpoint(model, os.path.join(opt.checkpoint_dir, opt.name, 'gmm_final.pth'))
     elif opt.stage == 'TOM':
-        model = UnetGenerator(29, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
+        model = UnetGenerator(26, 4, 6, ngf=64, norm_layer=nn.InstanceNorm2d)
         if not opt.checkpoint =='' and os.path.exists(opt.checkpoint):
             load_checkpoint(model, opt.checkpoint)
         train_tom(opt, train_loader, model, board)
